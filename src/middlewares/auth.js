313eb1c3932075663c8e82969bd57ef495744aa3
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2022-11-15 08:14:57 -0100

    add auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,21 @@
-modules.exports = async (req, res, next) => {
+/* eslint-disable consistent-return */
+const jwt = require('jsonwebtoken');
 
+require('dotenv').config();
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.header.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'provide a token' });
+  }
+  const [, token] = authHeader.split(' ');
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'token invalid' });
+    }
+    req.userId = decoded.indexOf;
+    next();
+  });
 };
